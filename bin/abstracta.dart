88879a8856b56dff1b1f7129bc8a5aa2369bde74
Carlos Soto <carlosf.soto1222@gmail.com>
2021-01-28 14:17:06 -0600

    Clase abstracta

---
+++ bin/abstracta.dart
@@ -0,0 +1,26 @@
+void main() {
+  final perro = Perro();
+  perro.emitirSonido();
+
+  final gato = Gato();
+  gato.emitirSonido();
+}
+
+abstract class Animal {
+  String nombre;
+
+  void emitirSonido();
+}
+
+class Perro implements Animal {
+  String nombre;
+  String raza;
+
+  void emitirSonido() => print("Guaauuu");
+}
+
+class Gato implements Animal {
+  String nombre;
+
+  void emitirSonido() => print("Miauuuuu");
+}
